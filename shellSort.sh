7e7be68692e592653430984beb67a457296fcd90
Tonyy-y <antonie.danoiu@gmail.com>
2025-12-07 21:04:29 +0200

    function shellSort() added

--- shellSort.sh
+++ shellSort.sh
@@ -1,15 +1,21 @@
 #!/bin/bash
-readarray -t v < numbers.in
-n=${#v[@]}
-for ((gap=n>>1; gap>=1; gap>>=1)); do
-    for ((i=gap; i<n; i++)); do
-        x=${v[$i]}
-        j=$i
-        while ((j>=gap && v[j-gap]>x)); do
-            v[$j]=${v[$j-$gap]}
-            ((j-=gap))
+
+shellSort() {
+    declare -n arr=$1
+    local n=${#arr[@]}
+    for ((gap=n>>1; gap>=1; gap>>=1)); do
+        for ((i=gap; i<n; i++)); do
+            local x=${arr[$i]}
+            local j=$i
+            while ((j>=gap && arr[j-gap]>x)); do
+                arr[$j]=${arr[$j-$gap]}
+                ((j-=gap))
+            done
+            arr[$j]=$x
         done
-        v[$j]=$x
     done
-done
+}
+
+readarray -t v < numbers.in
+shellSort v
 printf "%d\n" ${v[@]} > numbers.out
